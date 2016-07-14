04345c574b8253b831d9d46eda429237e251fa9b
Young-il Yoon <yoon_1318@naver.com>
2016-07-14 11:25:25 +0900

    initial commit

---
+++ KULibraryAE/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  KULibraryAE
+//
+//  Created by cscoi024 on 2016. 7. 14..
+//  Copyright © 2016년 youngil. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# KU-Library-ae
+고려대학교 안암캠퍼스 애기능 생활도서관 iOS App
