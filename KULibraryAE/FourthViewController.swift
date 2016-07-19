d2cb05f29004cb4560fc3a9e08105d17fda4ee5b
Young-il Yoon <yoon_1318@naver.com>
2016-07-19 17:38:17 +0900

    19.07.19 #2
    from 과학도서관 311호

--- KULibraryAE/FourthViewController.swift
+++ KULibraryAE/FourthViewController.swift
@@ -10,12 +10,9 @@ import UIKit
 
 class FourViewController: UIViewController {
     
-    @IBOutlet weak var text_name: UITextField!
-    @IBOutlet weak var text_phone: UITextField!
-    @IBOutlet weak var text_objective: UITextField!
-    @IBOutlet weak var text_date: UILabel!
-    @IBOutlet weak var text_begin_time: UILabel!
-    @IBOutlet weak var text_end_time: UILabel!
+    //@IBOutlet weak var text_name: UITextField!
+    //@IBOutlet weak var text_phone: UITextField!
+    //@IBOutlet weak var text_objective: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +20,8 @@ class FourViewController: UIViewController {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        text_name.resignFirstResponder()
-        text_phone.resignFirstResponder()
-        text_objective.resignFirstResponder()
-        
-        text_name.text = "윤영일(애생도)"
-        text_phone.text = "01096679647"
-        text_objective.text = "독서 세미나"
-        text_begin_time.text = "19:00"
-        text_begin_time.textColor = UIColor.darkGrayColor()
-        text_end_time.text = "21:00"
-        text_end_time.textColor = UIColor.darkGrayColor()
-    }
-    
-    @IBAction func schedule_onclick(sender: AnyObject) {
-        text_date.text = "7/20"
-        text_date.textColor = UIColor.darkGrayColor()
+        //text_name.resignFirstResponder()
+        //text_phone.resignFirstResponder()
+        //text_objective.resignFirstResponder()
     }
 }
